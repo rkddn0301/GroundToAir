817d761f83573cefc6c69791c4878e758a859119
rkddn0301 <rkddn0301@naver.com>
2024-10-07 23:33:29 +0900

    세계 공항 코드 테이블 수정으로 인해 컬럼 추가

--- backEnd/airReservation/src/main/java/groundToAir/airReservation/entity/IataCodeEntity.java
+++ backEnd/airReservation/src/main/java/groundToAir/airReservation/entity/IataCodeEntity.java
@@ -3,6 +3,7 @@ package groundToAir.airReservation.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+// 세계 공항코드 DB
 @Data
 @Entity
 @Table(name = "WORLD_AIRPORT_CODE")
@@ -21,8 +22,17 @@ public class IataCodeEntity {
     @Column(name="AIRPORT", nullable = false)
     private String airport;
 
+    @Column(name="AIRPORT_KOR", nullable = false)
+    private String airport_kor;
+
     @Column(name="COUNTRY", nullable = false)
     private String country;
+
+    @Column(name="COUNTRY_KOR", nullable = false)
+    private String country_kor;
+
+    @Column(name="REGION", nullable = false)
+    private String region;
 
     @Column(name="CITY", nullable = false)
     private String city;
